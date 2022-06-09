4a70c286c164dcc54dba9177bf23284af68911e0
Paula Gomez <paula_gomez13@hotmail.com>
2022-06-09 15:40:57 +0200

    handleEditNote

--- frontend/src/views/my-notes/note/Note.jsx
+++ frontend/src/views/my-notes/note/Note.jsx
@@ -6,6 +6,7 @@ import {
   deleteNote,
   getNote,
   updateNoteVisibility,
+  updateNote,
 } from "../../../services/notesService";
 import "./note.css";
 
@@ -16,18 +17,8 @@ export const Note = () => {
 
   const [note, setNote] = useState();
   const [isReadOnly, setIsReadOnly] = useState(true);
-
-  // Para editar
-  // Creamos un estado para saber si está editando no (hecho)
-  // Pasamos al botón el setIsReadOnly(!!isReadOnly)
-  // Si está a true - Mostramos el contenido estático
-  // Si está a false - Mostramps el contenido editable - TextArea
-  // Dos estados, uno para el título y otro para el contenido
-  // Al textArea en la función onChange le pasáis el e.current.value y vais seteándolo al estado
-  // Creamos un botón Guardar que se muestre solo si el isReadOnly = false
-  // Al hacer click en el botón hay que llamar al endpoint de editar nota y pasarle el contenido
-  // Y además seteamos la nota con el nuevo title y content (como en la visibilidad)
-  // Y seteamos el isReadOnly a true
+  const [title, setTitle] = useState();
+  const [content, setContent] = useState();
 
   useEffect(() => {
     const getNoteData = async () => {
@@ -54,7 +45,40 @@ export const Note = () => {
     getNoteData();
   }, []);
 
-  const changeVisibility = async () => {
+  // Para editar
+  // Creamos un estado para saber si está editando o no (hecho) X
+  // Pasamos al botón el setIsReadOnly(!!isReadOnly)
+  // Si está a true - Mostramos el contenido estático
+  // Si está a false - Mostramps el contenido editable - TextArea
+  // Dos estados, uno para el título y otro para el contenido
+  // Al textArea en la función onChange le pasáis el e.current.value y vais seteándolo al estado X
+  // Creamos un botón Guardar que se muestre solo si el isReadOnly = false
+  // Al hacer click en el botón hay que llamar al endpoint de editar nota y pasarle el contenido
+  // Y además seteamos la nota con el nuevo title y content (como en la visibilidad)
+  // Y seteamos el isReadOnly a true
+
+  const handleEditNote = async () => {
+    try {
+      await updateNote(params.id, title, content);
+      if (isReadOnly === "true") {
+      } else {
+        return (
+          <div>
+            <input type="text" id="titleNote" autoFocus />
+            <textarea
+              id="bodyNote"
+              onChange={(e) => setIsReadOnly(e.target.value)}
+            />
+            <button onClick={setNote}>Save</button>
+          </div>
+        );
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleVisibilityNote = async () => {
     try {
       const visibility = note.visibility === "public" ? "private" : "public";
       await updateNoteVisibility(params.id, visibility);
@@ -80,13 +104,13 @@ export const Note = () => {
         <button onClick={() => navigate(-1)}>
           <p>Atrás</p>
         </button>
-        <button>
+        <button onClick={handleEditNote}>
           <p>Editar</p>
         </button>
         <button onClick={handleDeleteNote}>
           <p>Borrar</p>
         </button>
-        <button onClick={changeVisibility}>
+        <button onClick={handleVisibilityNote}>
           <p>Visibilidad</p>
           <small>{note.visibility}</small>
         </button>
